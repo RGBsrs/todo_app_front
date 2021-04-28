df08a0066d147fa840d3b25aa31437e9d4551967
RGBSrs <grjanko.d.a@gmail.com>
2021-04-28 17:06:51 +0300

    add new views

--- src/components/marketing/LandingPage.vue
+++ src/components/marketing/LandingPage.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="flex-center">
         Landing
     </div>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -2,24 +2,43 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from '../App'
 import LandingPage from '../components/marketing/LandingPage'
+import About from '../components/marketing/About'
+import Register from '../components/auth/Register'
+import Login from '../components/auth/Login'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'LandingPage',
+    name: 'home',
     component: LandingPage
   },
   {
     path: '/todo',
-    name: 'App',
+    name: 'todo',
     component: App
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: About
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: Register
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
   },
 ]
 
 const router = new VueRouter({
-  routes
+  routes, 
+  mode: 'history'
 })
 
 export default router
